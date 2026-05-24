0280d38f8ad85f43feed1a3b76d732d4096f9fcb
2ec3 <eric-shao@hotmail.com>
2026-05-24 19:56:06 +0800

    add: linear_ps for eta

--- guided_diffusion/condition_methods.py
+++ guided_diffusion/condition_methods.py
@@ -167,7 +167,37 @@ class SinPosteriorSampling(ConditioningMethod):
         x_t = x_t - eta * norm_grad
 
         return x_t, norm
-    
+
+@register_conditioning_method(name='linear_ps')
+class LinearPosteriorSampling(ConditioningMethod):
+    def __init__(self, operator, noiser, **kwargs):
+        super().__init__(operator, noiser)
+        self.eta_min = kwargs.get('eta_min', 0)
+        self.eta_max = kwargs.get('eta_max', 5)
+        self.num_timesteps = kwargs.get('num_timesteps', 1000)
+
+    def get_adaptive_eta(self, t):
+        progress = 1.0 - t.float() / float(self.num_timesteps - 1)
+
+        eta = self.eta_min + (self.eta_max - self.eta_min) * progress
+
+        return eta
+
+    def conditioning(self, x_prev, x_t, x_0_hat, measurement, t=None, **kwargs):
+        norm_grad, norm = self.grad_and_value(x_prev=x_prev, x_0_hat=x_0_hat, measurement=measurement, **kwargs)
+
+        if t is None:
+            raise ValueError("AdaptivePosteriorSampling requires timestep t.")
+
+        eta = self.get_adaptive_eta(t)
+
+        while eta.ndim < norm_grad.ndim:
+            eta = eta.unsqueeze(-1)
+
+        x_t = x_t - eta * norm_grad
+
+        return x_t, norm
+
 @register_conditioning_method(name='ps+')
 class PosteriorSamplingPlus(ConditioningMethod):
     def __init__(self, operator, noiser, **kwargs):
